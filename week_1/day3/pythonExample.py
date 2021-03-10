b2a704cccd3b4d69e25bde13f36551ef31a38bf6
Calvin Kling <klingcal@gmail.com>
2021-03-10 11:48:55 -0800

    added list

--- week_1/day3/pythonExample.py
+++ week_1/day3/pythonExample.py
@@ -1,5 +1,11 @@
 greeting = "Hello DC family"
 classSize = 23
+floatExample = 2.232
+sumOfNumbers = 1 + 1
+variableName = True
+somethingToCallIt = ["students", 23, True]
+somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
 print("Hello world")
 print("My name is Calvin and I live in Atlanta")
 print(classSize)
+print(somethingToCallIt)
