c460f56caddb957f860bf7d84a203a20e8d7702a
Calvin Kling <klingcal@gmail.com>
2021-03-10 12:35:35 -0800

    added sum of class

--- week_1/day3/pythonExample.py
+++ week_1/day3/pythonExample.py
@@ -1,11 +1,14 @@
 greeting = "Hello DC family"
 classSize = 23
+dirPlusTeacher = 4
+sumOfClass = dirPlusTeacher + classSize
 floatExample = 2.232
 sumOfNumbers = 1 + 1
 variableName = True
 somethingToCallIt = ["students", 23, True]
 somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
-print("Hello world")
-print("My name is Calvin and I live in Atlanta")
-print(classSize)
-print(somethingToCallIt)
+# print("Hello world")
+# print("My name is Calvin and I live in Atlanta")
+# print(classSize)
+# print(somethingToCallIt)
+print("The size of our class is", sumOfClass, ".")
