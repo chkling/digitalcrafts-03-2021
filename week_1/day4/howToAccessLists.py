32b1d757230fc8a937de45d5b39f6f3e683dbfc6
Calvin Kling <klingcal@gmail.com>
2021-03-11 14:01:58 -0800

    initial commit

---
+++ week_1/day4/howToAccessLists.py
@@ -0,0 +1,21 @@
+#List
+pets = ["cat", "dog", "bunny"]
+
+#Access the list
+dog = pets[1]
+
+#Negative index to access the end of the list
+rabbit = pets[-1]
+
+#Multiple values - including first number and excluding last
+favoriteAnimals = pets[0:2]
+
+#if you start your index range with a negative, it starts from the end of the list 
+#and goes back however many times you want depending on the second number
+demonSpawns = pets[-2:-1]
+
+#Prints
+# print(dog)
+# print(rabbit)
+# print(favoriteAnimals)
+print(demonSpawns)
---
+++ week_1/day4/listsTuplesDictionaries.py
@@ -0,0 +1,25 @@
+#Lists - contained in square brackets
+aList = ["list item one", 2, 3, "fourth item"]
+
+#Dictionary - sequence of LABELED values
+#dictionary = {"string": "the value of string", 1:1, "mixture": 1, true: TRUE}
+dictionaryOfUsers = {
+    "name": "Calvin", 
+    "age": 29, 
+    "height": "6-0"
+    }
+
+#Dictionary AND list combo
+uesr = {
+    "name": "Calvin", 
+    "age": 29, 
+    "foods": ["eggs", "spaghetti"]
+    "placesLived": {
+        "US": "Atlanta",
+        "Europe": "Sweden",
+    },
+    }
+
+
+#Tuples
+tupleExample = (3333, 4444)
