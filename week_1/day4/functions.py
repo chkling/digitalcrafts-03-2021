e3d425c958602e3be9fe90f8a174c0aad98b472f
Calvin Kling <klingcal@gmail.com>
2021-03-11 08:37:06 -0800

    added functions

---
+++ week_1/day4/functions.py
@@ -0,0 +1,16 @@
+def add(number1, number2):
+    return number1 + number2
+
+def subtract(number1, number2):
+    return number1 - number2
+
+def multiply(number1, number2):
+    return number1 * number2
+
+def divide(number1, number2):
+    return number1 // number2
+
+# def lengthCalc(word):
+#     return print(len(word))
+
+# lengthCalc(input("Please provide a word: "))
