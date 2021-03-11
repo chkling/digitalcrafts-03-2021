52cf2a9978309090588e77f5b1f8eb3165501925
Calvin Kling <klingcal@gmail.com>
2021-03-11 12:16:16 -0800

    function and scope notes

--- week_1/day4/functions.py
+++ week_1/day4/functions.py
@@ -14,3 +14,8 @@ def divide(number1, number2):
 #     return print(len(word))
 
 # lengthCalc(input("Please provide a word: "))
+
+#SCOPE
+#the items inside the functions are not global
+#having access to certain things inside your program
+#global means anyone can access from anywhere inside the program
