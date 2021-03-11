833f458e9b75f17d6065c898ffa908110c57b440
Calvin Kling <klingcal@gmail.com>
2021-03-10 18:17:56 -0800

    formatting and confirmed valid input

--- week_1/day3/calculator.py
+++ week_1/day3/calculator.py
@@ -1,19 +1,40 @@
-firstNumber = int(input("Please enter a number: "))
-operand = input("Please enter a +, -, * or /: ")
-secondNumber = int(input("Please enter a second number: "))
+#first number
+firstNumber = input("Please enter a number: ")
+lengthFirstNumber = len(firstNumber)
 
+while(lengthFirstNumber < 1):
+    firstNumber = input("Please enter a number: ")
+    lengthFirstNumber = len(firstNumber)
+
+#operand
+operand = input("Please enter a +, -, * or /: ")
+lengthOperand = len(operand)
+
+while(lengthOperand < 1):
+    operand = input("Please enter a +, -, * or /: ")
+    lengthOperand = len(operand)
+
+#second number
+secondNumber = input("Please enter a second number: ")
+lengthSecondNumber = len(secondNumber)
+
+while(lengthSecondNumber < 1):
+    secondNumber = input("Please enter a second number: ")
+    lengthSecondNumber = len(secondNumber)
+
+#equation and output
 if(operand == "*"):
-    total = (firstNumber * secondNumber)
+    total = (int(firstNumber) * int(secondNumber))
     print("The result is %s." % (total))
 
 if(operand == "/"):
-    total = (firstNumber / secondNumber)
+    total = (int(firstNumber) / int(secondNumber))
     print("The result is %s." % (total))
 
 if(operand == "+"):
-    total = (firstNumber + secondNumber)
+    total = (int(firstNumber) + int(secondNumber))
     print("The result is %s." % (total))
 
 if(operand == "-"):
-    total = (firstNumber - secondNumber)
+    total = (int(firstNumber) - int(secondNumber))
     print("The result is %s." % (total))
