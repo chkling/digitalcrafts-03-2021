19989be11203df3125396693b0905e5337ef237d
Calvin Kling <klingcal@gmail.com>
2021-03-15 14:35:44 -0700

    dictionary created

---
+++ week_2/day1/dictionaryPractice.py
@@ -0,0 +1,47 @@
+# add your info
+digitalCraftsStudent = {
+    "name": "Calvin",
+    "city": "Atlanta",
+    "computer": ["Mac"],
+
+}
+# print out your computer information in the terminal
+print(digitalCraftsStudent["computer"])
+
+gamer = {
+    "platform": "PlayStation",
+    "gamingHours": [{"weekday": "9-5"}, {"weekends": "anytime"}],
+    "skill": "pwner"
+}
+# print preferred gaming hour
+print(gamer["gamingHours"][1]["weekends"])
+
+car = {
+    "model": "",
+    "image": "",
+    "year": "",
+    "engineChoices": [
+
+        {
+            "v4": {
+                "horsepower": 230
+            }
+        },
+        {
+            "v6": {
+                "horsepower": 300
+            }
+        },
+        {
+            "v8": {
+                "horsepower": 400
+            }
+        },
+        {
+            "v12": {
+                "horsepower": 900
+            }
+        }
+    ],
+
+}
