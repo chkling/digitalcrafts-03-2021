d9f14f1c7552f3eedff71f9066896b2706d3f785
Calvin Kling <klingcal@gmail.com>
2021-03-29 15:30:21 -0400

    calc

--- week_4/day1/examples/secondJSExamples.js
+++ week_4/day1/examples/secondJSExamples.js
@@ -19,8 +19,39 @@ let firstPizza = {
 };
 for (const pizza in firstPizza) {
 	// console.log(firstPizza[pizza]);
-	console.log(`This is the key: ${pizza}.`);
-	console.log(`This is the value: ${firstPizza[pizza]}.`);
-	console.log(Object.keys(firstPizza)); // returns an array, not ideal at this moment
+	// console.log(`This is the key: ${pizza}.`);
+	// console.log(`This is the value: ${firstPizza[pizza]}.`);
+	// console.log(Object.keys(firstPizza)); // returns an array, not ideal at this moment
 }
+// prints out just the toppings
+for (const topping of firstPizza.toppings) {
+	console.log(topping);
+}
+
+let cars = ["Suburu", "Audi", "Honda", "BMW"];
 // for of is for arrays
+console.log(cars[1]);
+for (const car of cars) {
+	console.log("This is a", car);
+}
+
+// while loop
+// while (condition) {
+// }
+
+// if statements
+// if(condition){
+
+// } else {
+
+// }
+
+// taking input
+// calculate("multiply", num1, num2)
+// print out the message of whatever operation they did and the correct answer is
+
+// calculate("multiply", 23, 84);
+// calculate("subtract", 23, 84);
+// add
+// divide
+// console log each to show it works
