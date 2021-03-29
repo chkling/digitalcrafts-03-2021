0f4a73d24914c98902246535fb840bded3b7e076
Calvin Kling <klingcal@gmail.com>
2021-03-29 14:57:06 -0400

    pizza ex

--- week_4/day1/examples/secondJSExamples.js
+++ week_4/day1/examples/secondJSExamples.js
@@ -1,6 +1,6 @@
 // String interpolation
-let height = "6ft";
-let weight = "140lbs";
+// let height = "6ft";
+// let weight = "140lbs";
 
 // method 1 of string interpolation
 // let JoesDimensions =
@@ -8,6 +8,19 @@ let weight = "140lbs";
 
 // method 2 of string interpolation
 // use backticks ` `
-let JoesDimensions = `Joe's height is ${height} and he weighs ${weight}.`;
+// let JoesDimensions = `Joe's height is ${height} and he weighs ${weight}.`;
 
-console.log(JoesDimensions);
+// for in is for objects, iterates over an object, returns the keys as strings
+let firstPizza = {
+	crust: "thin",
+	toppings: ["cheese", "pepperoni", "pineapple"],
+	size: "large",
+	sauce: ["marinara", "alfredo"],
+};
+for (const pizza in firstPizza) {
+	// console.log(firstPizza[pizza]);
+	console.log(`This is the key: ${pizza}.`);
+	console.log(`This is the value: ${firstPizza[pizza]}.`);
+	console.log(Object.keys(firstPizza)); // returns an array, not ideal at this moment
+}
+// for of is for arrays
