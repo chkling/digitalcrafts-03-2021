26ef4daf21ff235269d0b54ff795935479a2b5e0
Calvin Kling <klingcal@gmail.com>
2021-03-29 14:16:49 -0400

    second JS ex

---
+++ week_4/day1/examples/secondJSExamples.js
@@ -0,0 +1,13 @@
+// String interpolation
+let height = "6ft";
+let weight = "140lbs";
+
+// method 1 of string interpolation
+// let JoesDimensions =
+// 	"Joe's height is " + height + " and he weighs " + weight + ".";
+
+// method 2 of string interpolation
+// use backticks ` `
+let JoesDimensions = `Joe's height is ${height} and he weighs ${weight}.`;
+
+console.log(JoesDimensions);
