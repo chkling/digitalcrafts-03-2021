f29629f811e1752406949413e533b0c7e5bce50f
Calvin Kling <klingcal@gmail.com>
2021-03-29 10:50:18 -0400

    functions

--- week_4/day1/examples/introToJavascript.js
+++ week_4/day1/examples/introToJavascript.js
@@ -2,11 +2,33 @@
 // integer = 2;
 // boolean = False;
 
-// def sum(num1, num2):
-//     return num1 + num2;
-
 // while (pizza < 8):
 //     print("pizza")
 
 // for slice in pizza:
 //     print(slice)
+
+// Define a variable
+
+// var variable - DO NOT USE. has a functional scope
+var hello = "hello world";
+// ES6 - let has a block scope
+let integer = 2;
+// constant - cannot/should not be reassigned, has a block scope
+const boolean = false;
+
+// def sum(num1, num2):
+//     return num1 + num2;
+
+function sumNumbers(num1, num2) {
+	// function body, aka inside the curlies
+	//for loop syntax
+	// for (first arg: variable we define we initialize to 0)
+	// for (second arg: our loop condition, is your loop container)
+	// for (third arg: incrementer)
+	const pizza = 8;
+	for (let slice = 0; slice < pizza.length; slice++) {
+		const element = pizza[slice];
+	}
+	return num1 + num2;
+}
