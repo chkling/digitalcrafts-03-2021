b31767270dd071c4ceaf80f6faa543c2c5d5af81
Calvin Kling <klingcal@gmail.com>
2021-03-29 10:20:09 -0400

    javascript into

---
+++ week_4/day1/examples/introToJavascript.js
@@ -0,0 +1,12 @@
+// hello = "hello world";
+// integer = 2;
+// boolean = False;
+
+// def sum(num1, num2):
+//     return num1 + num2;
+
+// while (pizza < 8):
+//     print("pizza")
+
+// for slice in pizza:
+//     print(slice)
