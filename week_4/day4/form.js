e61358a5be837c4041800236369b791431bbb549
Calvin Kling <klingcal@gmail.com>
2021-04-01 12:47:30 -0400

    completed validation

--- week_4/day4/form.js
+++ week_4/day4/form.js
@@ -1,1 +1,19 @@
-console.log("Hey");
+const form = document.querySelector("form");
+
+const validateName = (name) => {
+	// console.log("Hi");
+	myRegex = /[A-Za-z]+/;
+	return myRegex.test(name);
+};
+
+form.addEventListener("submit", (event) => {
+	const textInput = document.querySelector("#textInput");
+	console.log(textInput.value);
+
+	if (!validateName(textInput.value)) {
+		event.preventDefault();
+		alert("Pleaes enter a valid name without any numbers.");
+	}
+});
+const calvin = 1;
+console.log(validateName(calvin));
