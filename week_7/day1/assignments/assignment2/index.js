4d2de078058471f451a14b4935e3102eea639c2d
Calvin Kling <klingcal@gmail.com>
2021-04-19 17:33:07 -0400

    updates

--- week_7/day1/assignments/assignment2/index.js
+++ week_7/day1/assignments/assignment2/index.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT = 3006;
+const PORT = 3008;
+const { readFile } = require("fs");
+app.use(express.json());
 
 app.get("/", (req, res) => {
-	ReadFile("index.html", "utf8", (err, html) => {
+	readFile(".index.html", "utf8", (err, html) => {
 		res.send(html);
 	});
 });
 
 app.get("/store", (req, res) => {
-	ReadFile("store.html", "utf8", (err, html) => {
+	readFile("store.html", "utf8", (err, html) => {
 		res.send(html);
 	});
+});
 
-	app.get("/contact", (req, res) => {
-		ReadFile("contact.html", "utf8", (err, html) => {
-			res.send(html);
-		});
+app.get("/contact", (req, res) => {
+	readFile("contact.html", "utf8", (err, html) => {
+		res.send(html);
 	});
+});
 
-	app.listen(PORT, () => {
-		console.log(`Running server on ${PORT}`);
-	});
+app.listen(PORT, () => {
+	console.log(`Running server on ${PORT}`);
 });
