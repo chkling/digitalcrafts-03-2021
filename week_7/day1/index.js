119da10300ca05c37479106f20d6b7ad37bdc23d
Calvin Kling <klingcal@gmail.com>
2021-04-19 12:41:10 -0400

    node

--- week_7/day1/index.js
+++ week_7/day1/index.js
@@ -1,16 +1,33 @@
-console.log("Hello");
-
 const http = require("http");
+const express = require("express");
+const app = express();
 const address = "127.0.0.1";
 const PORT = 3002;
+const { readFile } = require("fs");
+
+app.use(express.json());
 
 // const server = http.createServer((request,response))
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
-	res.end("Goodbye");
+// const server = http.createServer((req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader("Content-Type", "text/plain");
+// 	res.end("Goodbye");
+// });
+app.get("/", (req, res) => {
+	readFile("index.html", "utf8", (err, html) => {
+		res.send(html);
+	});
+});
+app.get("/home", (req, res) => {
+	readFile("homepage.html", "utf8", (err, html) => {
+		res.send(html);
+	});
 });
 
-server.listen(PORT, address, () => {
-	console.log(`server is on http://${address}:${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Your server is listening on ${PORT}`);
 });
+
+// server.listen(PORT, address, () => {
+// 	console.log(`server is on http://${address}:${PORT}`);
+// });
