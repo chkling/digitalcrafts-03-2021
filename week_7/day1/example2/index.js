4108dbdf787b6deaaa8ed93a168e4b0dfc3f86b9
Calvin Kling <klingcal@gmail.com>
2021-04-19 15:08:13 -0400

    more node

--- week_7/day1/example2/index.js
+++ week_7/day1/example2/index.js
@@ -1,3 +1,50 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const { readFile } = require("fs");
+
+// routes
+//1 default page
+//2 home page
+//3 about
+//4 FAQ
+
+// app.get("/", (req, res) => {
+// 	readFile("index.html", "utf8", (err, html) => {
+// 		res.send(html);
+// 	});
+// });
+
+app.get("/home", (req, res) => {
+	readFile("index.html", "utf8", (err, html) => {
+		res.send(html);
+	});
+});
+
+app.get("/about", (req, res) => {
+	readFile("about.html", "utf8", (err, html) => {
+		res.send(html);
+	});
+});
+
+app.get("/faq", (req, res) => {
+	readFile("faq.html", "utf8", (err, html) => {
+		res.send(html);
+	});
+});
+
+app.post("/messages", (req, res) => {
+	const message = "Post a message.";
+	res.send(message);
+});
+
+// star is the default when trying to go to a page that doesn't exist
+app.get("*", (req, res) => {
+	readFile("error.html", "utf8", (err, html) => {
+		res.send(html);
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Your server is being hosted on localhost: ${PORT}`);
+});
