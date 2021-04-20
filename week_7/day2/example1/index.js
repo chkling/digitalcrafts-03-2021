c7646c06955c5efad79be76d677eadab535686c9
Calvin Kling <klingcal@gmail.com>
2021-04-20 11:47:14 -0400

    assignment completed

--- week_7/day2/example1/index.js
+++ week_7/day2/example1/index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 3011;
-const { readFile } = require("fs");
+// const { readFile } = require("fs");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello");
+	res.send("Welcome Home");
+});
+
+app.get("/about", (req, res) => {
+	res.send("About this website");
+});
+
+app.get("/team", (req, res) => {
+	res.send("Meet the Team");
+});
+
+app.get("/faq", (req, res) => {
+	res.send("Q&A");
+});
+
+app.post("/faq", (req, res) => {
+	const { question, answer } = req.body;
+	const message = `Your question is ${question} and answer is ${answer}`;
+	res.send(message);
+});
+
+app.get("*", (req, res) => {
+	res.send("Page does not exist");
 });
 
 app.listen(PORT, () => {
-	console.log(`The server is on prt ${PORT}`);
+	console.log(`The server is on port ${PORT}`);
 });
