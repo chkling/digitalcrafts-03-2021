7e809626ea93605216459606d7087f9bd27bc730
Calvin Kling <klingcal@gmail.com>
2021-04-21 15:43:22 -0400

    updates

--- week_7/day3/index.js
+++ week_7/day3/index.js
@@ -12,6 +12,31 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the node server");
 });
 
+// routes we define for our node server
+
+// CRUD
+
+// create todo list
+app.post("/todo", async (req, res) => {
+	try {
+		const { description } = req.body;
+		const newTodoInDB = await pool.query(
+			"INSERT INTO todo (description) VALUES($1)",
+			[description]
+		);
+		console.log(req.body);
+		res.json(newTodoInDB);
+	} catch (err) {
+		console.log(err.message);
+	}
+});
+
+// read todo list
+
+// update todo list
+
+// delete the todo
+
 app.listen(port, () => {
 	console.log(`The server is on ${port}`);
 });
