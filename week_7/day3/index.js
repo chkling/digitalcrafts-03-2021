94e6800fe043be705c416926348dd74b9e2982c0
Calvin Kling <klingcal@gmail.com>
2021-04-21 16:19:40 -0400

    updates

--- week_7/day3/index.js
+++ week_7/day3/index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3015;
 const pool = require("./db.js");
-console.log(port);
 // middleware
 app.use(express.json());
 app.use(cors());
@@ -24,7 +23,6 @@ app.post("/todo", async (req, res) => {
 			"INSERT INTO todo (description) VALUES($1)",
 			[description]
 		);
-		console.log(req.body);
 		res.json(newTodoInDB);
 	} catch (err) {
 		console.log(err.message);
@@ -32,6 +30,30 @@ app.post("/todo", async (req, res) => {
 });
 
 // read todo list
+app.get("/read_todos", async (req, res) => {
+	try {
+		const readToDosFromDB = await pool.query("SELECT * from todo");
+		res.json(readToDosFromDB);
+	} catch (err) {
+		console.log(err.message);
+	}
+});
+
+// read specific todo
+app.get("/read_todos/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		// this looks at the url for whatever you put in :
+		console.log(id);
+		const readSingleToDoFromID = await pool.query(
+			"SELECT * from todo WHERE todo_id = ($1)",
+			[id]
+		);
+		res.json(readSingleToDoFromID);
+	} catch (err) {
+		console.log(err.message);
+	}
+});
 
 // update todo list
 
