149190b98df2f73b27e29c76d603d5e56cb63103
Calvin Kling <klingcal@gmail.com>
2021-04-24 14:31:08 -0400

    update

--- week_7/day5/algs.js
+++ week_7/day5/algs.js
@@ -20,21 +20,21 @@
 // Missing Digit
 
 // Write a function that when given a string equation with one digit as an 'x', return the value of 'x' that makes the equation true.
-const findX = (equation) => {
-	let problem = equation;
-	for (i = 0; i < problem.length; i++)
-		if (problem[i] == "x") {
-			let n = problem[i];
-			for (n = 0; n < 10; n++)
-				// let problem2 = parseInt(problem);
-				if (problem == true) return n;
-		}
-};
-// Part 1
-console.log(findX("10 * x = 20")); // returns 2
-console.log(findX("2 + 4 = x")); // returns 6
-console.log(findX("x4 - 11 = 33")); // returns 4
-// Part 2
-console.log(findX("1x0 * 2 = 200")); // returns 0
-console.log(findX("80 / 2 = x0")); // returns 4
-console.log(findX("111 + 11x = 222")); // returns 1
+// const findX = (equation) => {
+// 	let problem = equation;
+// 	for (i = 0; i < problem.length; i++)
+// 		if (problem[i] == "x") {
+// 			let n = problem[i];
+// 			for (n = 0; n < 10; n++)
+// 				// let problem2 = parseInt(problem);
+// 				if (problem == true) return n;
+// 		}
+// };
+// // Part 1
+// console.log(findX("10 * x = 20")); // returns 2
+// console.log(findX("2 + 4 = x")); // returns 6
+// console.log(findX("x4 - 11 = 33")); // returns 4
+// // Part 2
+// console.log(findX("1x0 * 2 = 200")); // returns 0
+// console.log(findX("80 / 2 = x0")); // returns 4
+// console.log(findX("111 + 11x = 222")); // returns 1
