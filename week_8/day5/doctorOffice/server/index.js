a00634d4f2eac72a2ce492c8e8e1bac2a7677fa6
Calvin Kling <klingcal@gmail.com>
2021-04-30 12:23:50 -0400

    c

--- week_8/day5/doctorOffice/server/index.js
+++ week_8/day5/doctorOffice/server/index.js
@@ -2,18 +2,42 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3333;
-// const es6Renderer = require("express-es6-template-engine");
-// const pool = require("./db.js");
+const es6Renderer = require("express-es6-template-engine");
+const { doctor, patient, appointment } = require("../models");
 
 app.engine("html", es6Renderer);
 app.set("views", "../client");
 app.set("view engine", "html");
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("home");
 });
+
+app.get("/doctors", async (req, res) => {
+	const doctorsFromDB = await doctor.findAll();
+	console.log(res.json(doctorsFromDB));
+	res.render("doctors", {
+		locals: {
+			doctors: doctorsFromDB,
+			title: "Doctor",
+		},
+	});
+});
+
+// app.post("/doctors", async (req, res) => {
+// 	const { name, email } = req.body;
+// 	const newDoctor = await doctor.create({
+// 		name,
+// 		email,
+// 	});
+
+// 	res.json({
+// 		id: newDoctor.id,
+// 	});
+// });
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
