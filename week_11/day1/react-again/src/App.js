e0cf43bf3be00ebccac4e0f38877d8a461b58a01
Calvin Kling <klingcal@gmail.com>
2021-05-17 12:15:56 -0400

    updates

--- week_11/day1/react-again/src/App.js
+++ week_11/day1/react-again/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Class from "./components/Class.js";
+import Class2 from "./components/Class2.js";
+import Class3 from "./components/Class3.js";
+import Class4 from "./components/Class4.js";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>App.js</h1>
+			<Class />
 		</div>
 	);
 }
