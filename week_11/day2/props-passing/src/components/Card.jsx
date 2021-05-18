f135d692dc1a6beb415a1f75344a144059fe3d02
Calvin Kling <klingcal@gmail.com>
2021-05-18 12:06:13 -0400

    filtered

--- week_11/day2/props-passing/src/components/Card.jsx
+++ week_11/day2/props-passing/src/components/Card.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import "./cardContainer.css";
 
 export default class Card extends Component {
+	state = {
+		flip: false,
+	};
 	render() {
-		const { hp, id, name } = this.props.pokemon;
-		return;
-		<div>
-			<p>{name}</p>
-			<p>{id}</p>
-			<p>{hp}</p>
-		</div>;
+		const { hp, id, name, sprites } = this.props.pokemon;
+		return (
+			<div className="pokemon-card">
+				<h1>Card</h1>
+				<img src={this.state.flip ? sprites.back : sprites.front} alt="" />
+				<h2>{name}</h2>
+				<p>{id}</p>
+				<p>{hp}</p>
+				<button onClick={() => this.setState({ flip: !this.state.flip })}>
+					Flip
+				</button>{" "}
+			</div>
+		);
 	}
 }
--- week_11/day2/props-passing/src/components/CardContainer.jsx
+++ week_11/day2/props-passing/src/components/CardContainer.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import Card from "./Card.jsx";
+import "./cardContainer.css";
 
 export default class CardContainer extends Component {
+	state = {
+		searchCriteria: "",
+	};
+
+	searchCriteria = (event) => {
+		this.setState({
+			searchCriteria: event.target.value,
+		});
+	};
+
 	render() {
 		const { pokemonData } = this.props;
-		console.log(pokemonData[0].pokemon);
+		const filteredData = pokemonData[0].pokemon.filter((pokemon) =>
+			pokemon.name.includes(this.state.searchCriteria)
+		);
+		console.log(filteredData);
 		return (
 			<div>
 				<h1>Card Container</h1>
-				{pokemonData[0].pokemon.map((singlePokemon, index) => {
-					<Card key={singlePokemon.name} pokemon={singlePokemon} />;
-				})}
+				<input
+					onChange={(e) => this.searchCriteria(e)}
+					type="text"
+					placeholder="Search for a pokemon"
+				/>
+				<button>Submit</button>
+				<div className="card-container">
+					{filteredData.map((singlePokemon, index) => (
+						<Card key={singlePokemon.name} pokemon={singlePokemon} />
+					))}
+				</div>
 			</div>
 		);
 	}
