2cc1e2dab4989734e427cab903ca1419e8cd00cb
Calvin Kling <klingcal@gmail.com>
2021-05-20 12:54:40 -0400

    updates

--- week_11/day4/functional-components/src/components/UserDetails.jsx
+++ week_11/day4/functional-components/src/components/UserDetails.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function UserDetails() {
+export default function UserDetails(props) {
 	return (
 		<div>
 			<h1>User Detail</h1>
+			<p>{props.firstName}</p>
 		</div>
 	);
 }
