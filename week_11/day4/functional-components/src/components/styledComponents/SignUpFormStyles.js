bf0726d2a1f428fd4c457ecd667eb1e3f2618b49
Calvin Kling <klingcal@gmail.com>
2021-05-20 12:39:16 -0400

    styled

--- week_11/day4/functional-components/src/components/styledComponents/SignUpFormStyles.js
+++ week_11/day4/functional-components/src/components/styledComponents/SignUpFormStyles.js
@@ -1,29 +1,28 @@
 import styled from "styled-components";
-
 export const SignUpButton = styled.button`
-background-color: #1fd1f9;
-import { withTheme } from "styled-components";
-background-image: linear-gradient(315deg, #1fd1f9 0%, #b621fe 74%);
-width: 200px;
-height: 40px;
-color: white;
-font-family: sans-ServiceUIFrameContext;
-font-size: 1/1rem;
-border-radius: 17px;
-border: 0;
-outline: none;
-cursor: pointer;
+	background-color: #1fd1f9;
+	background-image: linear-gradient(
+		315deg,
+		rgb(31, 209, 249) 0%,
+		rgb(182, 33, 254) 74%
+	);
+	width: 200px;
+	height: 40px;
+	color: white;
+	font-family: sans-serif;
+	font-size: 1.1rem;
+	border-radius: 17px;
+	border: 0;
+	cursor: pointer;
 `;
-
 export const SignUpFormInput = styled.input`
-height 2.2rem;
-width: 9rem;
-border-radius: 10px;
-outline: none;
-margin: 5px;
-padding: 5px;
+	height: 2.2rem;
+	width: 9rem;
+	border-radius: 16px;
+	padding-left: 20px;
+	margin-bottom: 1rem;
+	outline: none;
 `;
-
 export const SignUpFormDiv = styled.form`
 	display: grid;
 	place-items: center;
