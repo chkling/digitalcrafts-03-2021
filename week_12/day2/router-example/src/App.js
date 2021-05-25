d8890c73f4b9ff47013a71ed6e6913898d795c67
Calvin Kling <klingcal@gmail.com>
2021-05-25 11:42:26 -0400

    routes

--- week_12/day2/router-example/src/App.js
+++ week_12/day2/router-example/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Router>
+				<div>
+					<nav>
+						<Link to="/">Home</Link>
+						<Link to="/aboutme">About Me</Link>
+						<Link to="/blog">Blog</Link>
+					</nav>
+				</div>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/aboutme">
+						<AboutMe />
+					</Route>
+					<Route path="/blog">
+						<Blog />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
