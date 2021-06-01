891136ff748ff4abb183681206b946592f172606
Calvin Kling <klingcal@gmail.com>
2021-06-01 15:23:18 -0400

    updates

---
+++ week_13/day2/assignment/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form";
+import UserDetails from "./components/UserDetails";
+
+function App() {
+	return (
+		<div className="App">
+			<Form />
+			<UserDetails />
+		</div>
+	);
+}
+
+export default App;
